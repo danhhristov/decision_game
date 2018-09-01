377a8d48d901b7e1963ed0524409379c78cbeb63
antonOO <anton.o.obretenov@abv.bg>
2018-09-01 14:00:07 -0700

    Main camera changed; some movement added - it is backwards on the main road and it should be continuous

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    public Rail rail;
+
+    private int currentSeg;
+    private float transition;
+    private bool isCompleted;
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        if (!rail)
+            return;
+
+        if (!isCompleted)
+            Play();
+    }
+
+    public void Play()
+    {
+        transition += Time.deltaTime * 2;
+        if (transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+        }
+        else if (transition < 0)
+        {
+            transition = 1;
+            currentSeg--;
+        }
+
+        transform.position = rail.LinearPosition(currentSeg, transition);
+    }
+}
---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class Rail : MonoBehaviour
+{
+
+    private Transform[] nodes;
+
+    // Use this for initialization
+    void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+    }
+
+    public Vector3 LinearPosition(int seg, float ratio)
+    {
+        Vector3 p1 = nodes[seg].position;
+        Vector3 p2 = nodes[seg + 1].position;
+        Debug.Log(Vector3.Lerp(p1, p2, ratio).ToString());
+        return Vector3.Lerp(p1, p2, ratio);
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 1; i < nodes.Length - 1; i++)
+        {
+            Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3f);
+        }
+
+    }
+
+
+}
