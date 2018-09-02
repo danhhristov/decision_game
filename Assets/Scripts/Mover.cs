f65fb515a52b205507ac9c331db494f25ad8b5b5
antonOO <anton.o.obretenov@abv.bg>
2018-09-02 02:23:52 -0700

    locomotive moving up path

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -25,9 +25,6 @@ public class Mover : MonoBehaviour
 
     public void Play()
     {
-        if (rail.nodes.Length - 2 == currentSeg) {
-            return;
-        }
 
         transition += Time.deltaTime * 1/2;
         if (transition > 1)
@@ -36,7 +33,12 @@ public class Mover : MonoBehaviour
             transition = 0;
             currentSeg++;
         }
-        
+
+        if (rail.nodes.Length - 1 == currentSeg)
+        {
+            return;
+        }
+
         transform.position = rail.LinearPosition(currentSeg, transition);
     }
 }
