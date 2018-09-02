6283c844d61fc658e44862c8d80c92c3190b8ba7
antonOO <anton.o.obretenov@abv.bg>
2018-09-02 01:31:28 -0700

    Changed scripts so they move through the whole line

--- Assets/Scripts/Rail.cs
+++ Assets/Scripts/Rail.cs
@@ -7,25 +7,25 @@ using UnityEditor;
 public class Rail : MonoBehaviour
 {
 
-    private Transform[] nodes;
+    public Transform[] nodes;
 
     // Use this for initialization
     void Start()
     {
         nodes = GetComponentsInChildren<Transform>();
+        Debug.Log(nodes.Length);
     }
 
     public Vector3 LinearPosition(int seg, float ratio)
     {
         Vector3 p1 = nodes[seg].position;
         Vector3 p2 = nodes[seg + 1].position;
-        Debug.Log(Vector3.Lerp(p1, p2, ratio).ToString());
         return Vector3.Lerp(p1, p2, ratio);
     }
 
     private void OnDrawGizmos()
     {
-        for (int i = 1; i < nodes.Length - 1; i++)
+        for (int i = 1; i < nodes.Length - 2; i++)
         {
             Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3f);
         }
